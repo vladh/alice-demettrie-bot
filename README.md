9b5d51da8e1d0ae9632fab913f51b9a47bdc8c42
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-12-30 12:25:07 +0100

    make it do the thing I wanted it to do

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ A simple Telegram bot for vladh.
 ## How to run
 
 Copy `run-example.sh` to `run.sh` and add your bot key.
+
+## Commands
+
+```
+* setvol xx%: Set volume to xx percent.
+* help: Print the help text
+```
--- main.go
+++ main.go
@@ -1,11 +1,63 @@
 package main
 
 import (
+  "fmt"
   "log"
   "os"
+  "os/exec"
+  "regexp"
+  "strconv"
+  "strings"
 
   tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+
+const HELP_TEXT = `Here's some help to get you started.
+
+* setvol xx%: Set volume to xx percent.
+* help: Print the help text
+`
+
+
+func processMessage(update tgbotapi.Update) string {
+  if strings.HasPrefix(update.Message.Text, "setvol") {
+    r := regexp.MustCompile(`setvol ([0-9]+)%`)
+    submatch := r.FindStringSubmatch(update.Message.Text)
+    if len(submatch) != 2 {
+      return "Wait, what do you mean? (usage: setvol xx%)"
+    }
+
+    percentage, err := strconv.Atoi(submatch[1])
+    if err != nil {
+      return "Wait, what do you mean? (usage: setvol xx%)"
+    }
+
+    vol := int((float64(percentage) / 100.0) * 65535.0)
+    cmd := exec.Command("nircmd", "setsysvolume", strconv.Itoa(vol))
+    err = cmd.Run()
+
+    if err != nil {
+      return "That didn't work (error running volume command)"
+    }
+
+    return fmt.Sprintf("Okay, set volume to %d%% (%d)", percentage, vol)
+  } else if strings.HasPrefix(update.Message.Text, "sleep") {
+    cmd := exec.Command("nircmd", "standby")
+    err := cmd.Run()
+
+    if err != nil {
+      return "That didn't work (error running sleep command)"
+    }
+
+    return "Nighty night (sleeping)"
+  } else if strings.HasPrefix(update.Message.Text, "help") {
+    return HELP_TEXT
+  }
+
+  return "Whatever you say :) (command not found)"
+}
+
 
 func main() {
   bot, err := tgbotapi.NewBotAPI(os.Getenv("ALICE_TOKEN"))
@@ -23,12 +75,10 @@ func main() {
   updates := bot.GetUpdatesChan(u)
 
   for update := range updates {
-    if update.Message != nil { // If we got a message
-      log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-      msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+    if update.Message != nil {
+      replyText := processMessage(update)
+      msg := tgbotapi.NewMessage(update.Message.Chat.ID, replyText)
       msg.ReplyToMessageID = update.Message.MessageID
-
       bot.Send(msg)
     }
   }
